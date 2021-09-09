fe668973d6d0a931baca014bee376de8dde96a7b
byMaxKucher <63538024+MaxDevIos@users.noreply.github.com>
2021-09-09 10:45:53 +0300

    Initial Commit

---
+++ RandomValueFromRange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomValueFromRange
+//
+//  Created by Maxim Kucherov on 09.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
